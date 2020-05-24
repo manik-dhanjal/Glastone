f0b4af449801df574c555ea0413161b1a2823696
techbydev <devender.sh97@gmail.com>
2020-05-24 23:14:00 +0530

    Added_Slider

--- src/components/sectionProjects.js
+++ src/components/sectionProjects.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React,{Component} from "react"
 
-const SectionProjects=()=>{
-    return(
-        <div>
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+export default function SectionProjects() {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows:true,
+    centerPadding:50,
+    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <div>
+      <div>
           <section className="section section-content section_pb-xsmall bg-dark text-center "style={{visibility: "inherit",opacity: "1"}} data-os-animation="data-os-animation">
         <div className="container">
           <div className="row justify-content-center">
@@ -15,128 +58,29 @@ const SectionProjects=()=>{
           </div>
         </div>
       </section>
- <section className="section section-portfolio bg-dark">
-        <div className="swiper-container slider slider-projects js-slider-projects">
-          <div className="swiper-wrapper">
-            {/* <div className="swiper-slide"><a className="figure-portfolio figure-portfolio-item_hover" href="project-1.html" data-pjax-link="flyingHeading">
-                <div className="figure-portfolio__wrapper-img">
-                  <div ><img src="img/projects/bg-1-thumb_vertical.jpg" alt width={1200} height={1800} /></div>
-                  <div className="figure-portfolio__content bg-dark-3">
-                    <div className="figure-portfolio__category">House Interior</div>
-                    <div className="figure-portfolio__header">
-                      <h3 className="figure-portfolio__heading split-text js-text-to-fly js-split-text" data-split-text-type="lines, words, chars" data-split-text-set="chars">Luxury Touch</h3>
-                      <div className="figure-portfolio__icon material-icons">keyboard_arrow_right</div>
-                    </div>
-                    <div className="figure-portfolio__curtain bg-dark-3" />
-                  </div>
-                </div></a>
-            </div>
-            <div className="swiper-slide"><a className="figure-portfolio figure-portfolio-item_hover" href="project-2.html" data-pjax-link="flyingHeading">
-                <div className="figure-portfolio__wrapper-img">
-                  <div ><img src="img/projects/bg-2-thumb_vertical.jpg"  alt width={1200} height={1800} /></div>
-                  <div className="figure-portfolio__content bg-dark-3">
-                    <div className="figure-portfolio__category">Interior Design</div>
-                    <div className="figure-portfolio__header">
-                      <h3 className="figure-portfolio__heading split-text js-text-to-fly js-split-text" data-split-text-type="lines, words, chars" data-split-text-set="chars">Platinum Perfection</h3>
-                      <div className="figure-portfolio__icon material-icons">keyboard_arrow_right</div>
-                    </div>
-                    <div className="figure-portfolio__curtain bg-dark-3" />
-                  </div>
-                </div></a>
-            </div>
-            <div className="swiper-slide"><a className="figure-portfolio figure-portfolio-item_hover" href="project-3.html" data-pjax-link="flyingHeading">
-                <div className="figure-portfolio__wrapper-img">
-                  <div className="lazy"><img src="img/projects/bg-3-thumb_vertical.jpg" alt width={1200} height={1800} /></div>
-                  <div className="figure-portfolio__content bg-dark-3">
-                    <div className="figure-portfolio__category">Hotel Interior</div>
-                    <div className="figure-portfolio__header">
-                      <h3 className="figure-portfolio__heading split-text js-text-to-fly js-split-text" data-split-text-type="lines, words, chars" data-split-text-set="chars">Royal King Suite</h3>
-                      <div className="figure-portfolio__icon material-icons">keyboard_arrow_right</div>
-                    </div>
-                    <div className="figure-portfolio__curtain bg-dark-3" />
-                  </div>
-                </div></a>
-            </div>
-            <div className="swiper-slide"><a className="figure-portfolio figure-portfolio-item_hover" href="project-4.html" data-pjax-link="flyingHeading">
-                <div className="figure-portfolio__wrapper-img">
-                  <div className="lazy"><img data-src="img/assets/projects/bg-4-thumb_vertical.jpg" src="#" alt width={1200} height={1800} /></div>
-                  <div className="figure-portfolio__content bg-dark-3">
-                    <div className="figure-portfolio__category">Apartments Interior</div>
-                    <div className="figure-portfolio__header">
-                      <h3 className="figure-portfolio__heading split-text js-text-to-fly js-split-text" data-split-text-type="lines, words, chars" data-split-text-set="chars">Elegant Apartment</h3>
-                      <div className="figure-portfolio__icon material-icons">keyboard_arrow_right</div>
-                    </div>
-                    <div className="figure-portfolio__curtain bg-dark-3" />
-                  </div>
-                </div></a>
-            </div>
-            <div className="swiper-slide"><a className="figure-portfolio figure-portfolio-item_hover" href="project-5.html" data-pjax-link="flyingHeading">
-                <div className="figure-portfolio__wrapper-img">
-                  <div className="lazy"><img data-src="img/assets/projects/bg-5-thumb_vertical.jpg" src="#" alt width={1200} height={1800} /></div>
-                  <div className="figure-portfolio__content bg-dark-3">
-                    <div className="figure-portfolio__category">Landscape Design</div>
-                    <div className="figure-portfolio__header">
-                      <h3 className="figure-portfolio__heading split-text js-text-to-fly js-split-text" data-split-text-type="lines, words, chars" data-split-text-set="chars">European Style</h3>
-                      <div className="figure-portfolio__icon material-icons">keyboard_arrow_right</div>
-                    </div>
-                    <div className="figure-portfolio__curtain bg-dark-3" />
-                  </div>
-                </div></a>
-            </div>
-            <div className="swiper-slide"><a className="figure-portfolio figure-portfolio-item_hover" href="project-6.html" data-pjax-link="flyingHeading">
-                <div className="figure-portfolio__wrapper-img">
-                  <div className="lazy"><img data-src="img/assets/projects/bg-6-thumb_vertical.jpg" src="#" alt width={1200} height={1800} /></div>
-                  <div className="figure-portfolio__content bg-dark-3">
-                    <div className="figure-portfolio__category">Apartments Interior</div>
-                    <div className="figure-portfolio__header">
-                      <h3 className="figure-portfolio__heading split-text js-text-to-fly js-split-text" data-split-text-type="lines, words, chars" data-split-text-set="chars">Loft Apartment</h3>
-                      <div className="figure-portfolio__icon material-icons">keyboard_arrow_right</div>
-                    </div>
-                    <div className="figure-portfolio__curtain bg-dark-3" />
-                  </div>
-                </div></a>
-            </div> */}
-          </div>
-          <div className="container slider__footer-relative">
-            <div className="row align-items-center justify-content-between">
-              <div className="col-auto col-md-4 text-left slider__footer-col">
-                <div className="slider__arrows">
-                  <div className="slider__arrow js-slider-projects__prev"><i className="material-icons">&#x2190;</i></div>
-                  <div className="slider__arrows-divider" />
-                  <div className="slider__arrow js-slider-projects__next"><i className="material-icons">&#x2192;</i></div>
-                </div>
-              </div>
-              {/* - slider nav arrows */}
-              <div className="col-auto col-md-4 text-right slider__footer-col">
-                <div className="slider__progress">
-                  <div className="swiper-container slider__counter slider__counter_current js-slider-projects__counter-current">
-                    <div className="swiper-wrapper" />
-                  </div>
-                  <div className="slider__counter-divider" />
-                  <div className="slider__counter slider__counter_total js-slider-projects__counter-total">001</div>
-                </div>
-              </div>
-              {/* - slider counter */}
-            </div>
-          </div>
-        </div>
-      </section>
-      
-      <section className="section section-content section_pt section_pb-xsmall bg-dark color-white text-center" data-os-animation="data-os-animation">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-lg-8 section-content__header">
-              <div className="section-content__subheading subheading split-text js-split-text" data-split-text-type="lines" data-split-text-set="lines"><span>Award winning agency</span></div>
-              <div className="split-text js-split-text" data-split-text-type="lines" data-split-text-set="lines">
-                <h3>Honors &amp; Mentions</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-        </div>
-       
-    )
-}
 
-export default SectionProjects
+      <Slider {...settings}>
+        <div>
+          <h3><img src="img/INTERIORS - SPR 1.jpg"></img></h3>
+        </div>
+        <div>
+          <h3><img src="img/WATER BODIES - SPR2.jpg" ></img></h3>
+        </div>
+        <div>
+          <h3><img src="img/SWIMMING POOLS - SPR3.jpg" ></img></h3>
+        </div>
+        <div>
+        <h3><img src="img/EXTERIORS - SPR4.jpg" ></img></h3>
+        </div>
+        <div>
+        <h3><img src="img/WATER BODIES - SPR5.jpg" ></img></h3>
+        </div>
+        <div>
+        <h3><img src="img/EXTERIORS - SPR6.jpg" style={{width:'345px',height:'517px'}} ></img></h3>
+        </div>
+      
+      </Slider>
+    </div>
+    </div>
+  );
+  };
